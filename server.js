00bf204dc32720ec3c1d097cba00e8a06e07099c
Miroslaw Kucharzyk <miroslaw.kucharzyk@schibsted.pl>
2015-12-29 09:24:53 +0100

    env variables

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-var url = 'mongodb://localhost:27017/library';
+var url = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/library';
 var db = require('./db')(url);
 var routes = require('./routes')(db);
 var app = require('./app')(routes);
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
